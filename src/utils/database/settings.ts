234347211d1eabed72271495727616d18f7fd31d
Zaka <zakahacecosas@protonmail.com>
2025-02-10 17:06:51 +0100

    i forgor

--- src/utils/database/settings.ts
+++ src/utils/database/settings.ts
@@ -222,13 +222,13 @@ export const settingsDefinition: Record<
     settings: {
       join_text: {
         type: "TEXT",
-        desc: "Text sent when a user joins. You can use (codes) to add dynamic info, run /help codes for info.",
+        desc: "Text sent when a user joins. Use (variables) to add dynamic info, run /help variables for info.",
         val: "Welcome to (servername), (name)! Interestingly, you just helped us reach (count) members. Have a nice day!",
         emoji: "🍍",
       },
       leave_text: {
         type: "TEXT",
-        desc: "Text sent when a user leaves. You can use (codes) to add dynamic info, run /help codes for info.",
+        desc: "Text sent when a user leaves. Use (variables) to add dynamic info, run /help variables for info.",
         val: "(name) has left the server! 😥",
         emoji: "🍍",
       },
