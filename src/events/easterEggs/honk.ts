c182bf1cf0b9624dbabf7b83cb9d8da046da105c
userandaname <zayaan.rajput@gmail.com>
2025-04-17 19:16:20 +0100

    fixed for ghostbusters

--- src/events/easterEggs/honk.ts
+++ src/events/easterEggs/honk.ts
@@ -1,7 +1,7 @@
 import type { Message, TextChannel } from "discord.js";
 
 export async function run(message: Message) {
-  const honks = ["hnok", "hokn", "hkon", "onk", "hon", "honhk", "hhonk", "honkk", "honk"]; // there is no reason why honk can't be part of this list, stop discrimination smh
+  const honks = ["hnok", "hokn", "hkon", "onk", "hon", "honhk", "hhonk", "honkk"]; // discrimination is legal, honk is NOT allowed here
   if (!honks.includes(message.content.trim().toLowerCase())) return;
   (message.channel as TextChannel).send("https://tenor.com/bW8sm.gif");
 }
