8cb03e1b73550794732efd0392ee5901cefc7bdc
ZakaHaceCosas <zakahacecosas@protonmail.com>
2025-06-28 23:31:37 +0200

    /moderation can now filter by case type

--- src/commands/moderation/cases.ts
+++ src/commands/moderation/cases.ts
@@ -1,10 +1,16 @@
-import { getModeration, listGuildModeration, listUserModeration } from "database/moderation";
+import {
+  getModeration,
+  listGuildModeration,
+  listUserModeration,
+  modType,
+} from "database/moderation";
 import {
   EmbedBuilder,
   SlashCommandSubcommandBuilder,
   type ChatInputCommandInteraction,
 } from "discord.js";
 import { errorEmbed } from "embeds/errorEmbed";
+import { capitalize } from "utils/capitalize";
 import { genColor } from "utils/colorGen";
 import { pfpCheck } from "utils/pfpCheck";
 import { randomize } from "utils/randomize";
@@ -15,6 +21,41 @@ export const data = new SlashCommandSubcommandBuilder()
   .addUserOption(user => user.setName("user").setDescription("The user that you want to see."))
   .addStringOption(string =>
     string.setName("id").setDescription("The ID of a specific moderation case you want to see."),
+  )
+  .addStringOption(string =>
+    string
+      .setName("type")
+      .setDescription("The specific type of action you'd like to see.")
+      .setChoices(
+        {
+          name: "Bans",
+          value: "BAN",
+        },
+        {
+          name: "Unbans",
+          value: "UNBAN",
+        },
+        {
+          name: "Warnings",
+          value: "WARN",
+        },
+        {
+          name: "Kicks",
+          value: "KICK",
+        },
+        {
+          name: "Mutes",
+          value: "MUTE",
+        },
+        {
+          name: "Unmutes",
+          value: "UNMUTE",
+        },
+        {
+          name: "Notes",
+          value: "NOTE",
+        },
+      ),
   );
 
 export async function run(interaction: ChatInputCommandInteraction) {
@@ -43,6 +84,7 @@ export async function run(interaction: ChatInputCommandInteraction) {
 
   const user = interaction.options.getUser("user");
   let actionID = interaction.options.getString("id");
+  const type = interaction.options.getString("type");
   if (actionID && actionID?.startsWith("#")) actionID = actionID.slice(1);
   if (actionID && !user)
     return await errorEmbed({
@@ -52,7 +94,9 @@ export async function run(interaction: ChatInputCommandInteraction) {
       reason: `Sokora cannot look for "case ${actionID} of *no user*". Please, specify a user.`,
     });
   if (!user) {
-    const cases = listGuildModeration(guild.id);
+    const cases = type
+      ? listGuildModeration(guild.id, type as modType)
+      : listGuildModeration(guild.id);
     if (cases.length === 0) {
       await interaction.reply({
         embeds: [
@@ -78,13 +122,16 @@ export async function run(interaction: ChatInputCommandInteraction) {
         value: [
           `\`Case ${c.id} for user\` <@${c.user}>`,
           `**Moderator**: <@${c.moderator}>`,
+          type ? "" : `**Action**: ${c.type}`,
           c.reason ? `**Reason**: ${c.reason}` : "*No reason provided*",
           `**Time of action**: <t:${Math.floor(Number(c.timestamp) / 1000)}:d>`,
-        ].join("\n"),
+        ]
+          .filter(Boolean)
+          .join("\n"),
       };
     });
     const embed = new EmbedBuilder()
-      .setAuthor({ name: "All moderation cases server-wide" })
+      .setAuthor({ name: `All ${type ? type.toLowerCase() : "moderation"} cases server-wide` })
       .setFields(fields)
       .setColor(genColor(0));
     await interaction.reply({ embeds: [embed] });
@@ -92,12 +139,14 @@ export async function run(interaction: ChatInputCommandInteraction) {
   }
   const actions = actionID
     ? getModeration(guild.id, user!.id, actionID)
-    : listUserModeration(guild.id, user!.id);
+    : type
+      ? listUserModeration(guild.id, user!.id, type as modType)
+      : listUserModeration(guild.id, user!.id);
 
   const avatar = user.displayAvatarURL();
   const embed = new EmbedBuilder()
     .setAuthor({
-      name: `${pfpCheck(avatar)}Cases of ${user.username}`,
+      name: `${pfpCheck(avatar)}${type ? `${capitalize(type.toLowerCase())} cases` : "Cases"} of ${user.username}`,
       iconURL: avatar,
     })
     .setFields(
