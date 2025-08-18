b66f1ab7f9ba39b58d7fb06f74c2147c2f4d08f8
ZakaHaceCosas <zakahacecosas@protonmail.com>
2025-08-18 21:16:03 +0200

    newline fix

--- src/utils/newline.ts
+++ src/utils/newline.ts
@@ -1,6 +1,6 @@
 // I have a love-hate relationship with this function
 // (mostly hate)
-export function newline(str: string, chars: number, startWith?: string): string {
+export function newline(str: string, chars: number, startWith: string = ""): string {
   const splitStr = str.split(" ");
   const array: string[] = [];
   let length: number = 0;
@@ -8,6 +8,10 @@ export function newline(str: string, chars: number, startWith?: string): string 
 
   splitStr.map(string => {
     let word = string.concat(" ");
+    if (word.trim().startsWith("**")) {
+      word = `\n${word}`
+      length -= (word.length + startWith.length)
+    }
     length += word.length;
     while (length > chars + i) {
       if (length > chars) word = `\n${word}`;
