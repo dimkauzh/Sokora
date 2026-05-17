21cc74cacb16d447251d9dbeb9c67c975011bc31
Golem64 <65229557+Golem642@users.noreply.github.com>
2026-05-17 16:08:37 +0200

    [fix] Building typescript errors

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -15,3 +15,12 @@ export type ReplaceableStrings =
 export type Replacements = { text: ReplaceableStrings; replacement: string | number }[];
 
 export type Satisfies<K, T extends K> = T;
+
+/**
+ * Force typescript to recognize a variable as a certain type (useful for polymorphic const variables for example)
+ * @param v The variable you want to force the type of
+ * @returns true (the variable is now of the type specified in `<T>`)
+ */
+export function forceType<T>(v: any): v is T {
+  return true;
+}
