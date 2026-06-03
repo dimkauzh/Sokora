06b040ea293bfe1e69c4510871ce9835e95e5000
ZakaHaceCosas <zakahacecosas@protonmail.com>
2026-06-03 16:09:35 +0200

    [fix@db] i'm actually stupid wait

--- migrations/init.sql
+++ migrations/init.sql
@@ -51,9 +51,9 @@ CREATE TABLE IF NOT EXISTS starboard (
   "timestamp" TIMESTAMP
 );
 
-ALTER TABLE settings ADD CONSTRAINT settings_pk PRIMARY KEY ("guild_id", "key");
-ALTER TABLE user_settings ADD CONSTRAINT user_settings_pk PRIMARY KEY ("user_id", "key");
-ALTER TABLE leveling ADD CONSTRAINT leveling_pk PRIMARY KEY ("guild_id", "user_id");
-ALTER TABLE moderation ADD CONSTRAINT moderation_pk PRIMARY KEY ("guild_id", "id");
-ALTER TABLE news ADD CONSTRAINT news_pk PRIMARY KEY ("guild_id", "id");
-ALTER TABLE starboard ADD CONSTRAINT starboard_pk PRIMARY KEY ("guild_id", "message");
+ALTER TABLE settings ADD CONSTRAINT settings_pk PRIMARY KEY ("guildID", "key");
+ALTER TABLE user_settings ADD CONSTRAINT user_settings_pk PRIMARY KEY ("userID", "key");
+ALTER TABLE leveling ADD CONSTRAINT leveling_pk PRIMARY KEY ("guild", "userID");
+ALTER TABLE moderation ADD CONSTRAINT moderation_pk PRIMARY KEY ("guild", "id");
+ALTER TABLE news ADD CONSTRAINT news_pk PRIMARY KEY ("guildID", "id");
+ALTER TABLE starboard ADD CONSTRAINT starboard_pk PRIMARY KEY ("guild", "message");
